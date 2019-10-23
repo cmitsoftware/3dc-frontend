a92d056867171e8eff79c64dd0ba45a453d81710
cmitsoftware <cmit.software@gmail.com>
2019-10-23 07:20:03 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// components
+import Layout from "./Layout";
+
+// pages
+import Error from "../pages/error";
+import Login from "../pages/login";
+
+// context
+import { useUserState } from "../context/UserContext";
+
+export default function App() {
+  // global
+  var { isAuthenticated } = useUserState();
+
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/3dc/dashboard" />} />
+        <Route exact path="/3dc" render={() => <Redirect to="/3dc/dashboard" />} />
+        <PrivateRoute path="/3dc" component={Layout} />
+        <PublicRoute path="/login" component={Login} />
+        <Route component={Error} />
+      </Switch>
+    </HashRouter>
+  );
+
+  // #######################################################################
+
+  function PrivateRoute({ component, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          isAuthenticated ? (
+            React.createElement(component, props)
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
+
+  function PublicRoute({ component, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          isAuthenticated ? (
+            <Redirect
+              to={{
+                pathname: "/",
+              }}
+            />
+          ) : (
+            React.createElement(component, props)
+          )
+        }
+      />
+    );
+  }
+}
